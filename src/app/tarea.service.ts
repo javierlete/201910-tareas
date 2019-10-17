50ee9030708019419b1695bfd1b1ee266b3a0c46
Javier Lete <javier@eskura21.com>
2019-10-17 20:35:34 +0200

    Servicio con getTareas

--- src/app/tarea.service.ts
+++ src/app/tarea.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Tarea } from './tarea';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
-  constructor() { }
+  tareas: Tarea[] = [
+    { id: 1, nombre: 'Tarea 1', descripcion: 'Descripción 1', terminada: false },
+    { id: 2, nombre: 'Tarea 2', descripcion: 'Descripción 2', terminada: true }
+  ];
+
+  getTareas(): Observable<Tarea[]> {
+    return of(this.tareas);
+  }
 }
--- src/app/tareas-tabla/tareas-tabla.component.ts
+++ src/app/tareas-tabla/tareas-tabla.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarea } from '../tarea';
+import { TareaService } from '../tarea.service';
 
 @Component({
   selector: 'app-tareas-tabla',
   templateUrl: './tareas-tabla.component.html',
   styleUrls: ['./tareas-tabla.component.css']
 })
-export class TareasTablaComponent {
-  tareas: Tarea[] = [
-    { id: 1, nombre: 'Tarea 1', descripcion: 'Descripción 1', terminada: false },
-    { id: 2, nombre: 'Tarea 2', descripcion: 'Descripción 2', terminada: true }
-  ];
+export class TareasTablaComponent implements OnInit {
+  tareas: Tarea[];
+
+  constructor(private tareaService: TareaService) {}
+
+  ngOnInit(): void {
+    this.tareaService.getTareas().subscribe(
+      tareasRecibidas => this.tareas = tareasRecibidas
+    );
+  }
 
   onBorrar() {
     if (confirm('¿Estás seguro de que quieres borrar este registro?')) {

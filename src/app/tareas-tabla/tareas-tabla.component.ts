46e0054f47dff1e9de7dbd7b80bf95dc2050f1b2
Javier Lete <javier@eskura21.com>
2019-10-17 20:16:08 +0200

    Simulación de borrado con confirmación

--- src/app/tareas-tabla/tareas-tabla.component.ts
+++ src/app/tareas-tabla/tareas-tabla.component.ts
@@ -11,4 +11,12 @@ export class TareasTablaComponent {
     { id: 1, nombre: 'Tarea 1', descripcion: 'Descripción 1', terminada: false },
     { id: 2, nombre: 'Tarea 2', descripcion: 'Descripción 2', terminada: true }
   ];
+
+  onBorrar() {
+    if (confirm('¿Estás seguro de que quieres borrar este registro?')) {
+      alert('Registro borrado');
+    } else {
+      alert('El registro se conservará');
+    }
+  }
 }

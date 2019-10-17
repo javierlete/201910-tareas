bb367e723031d9ba58575c044644737ec58a40dc
Javier Lete <javier@eskura21.com>
2019-10-17 19:41:54 +0200

    ng g c tarea-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TareasTablaComponent } from './tareas-tabla/tareas-tabla.component';
+import { TareaFormComponent } from './tarea-form/tarea-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TareasTablaComponent
+    TareasTablaComponent,
+    TareaFormComponent
   ],
   imports: [
     BrowserModule,

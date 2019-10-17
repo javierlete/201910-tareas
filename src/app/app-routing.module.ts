c8967134e34c4b12aaa40f17099d903ec61adb5c
Javier Lete <javier@eskura21.com>
2019-10-17 19:47:05 +0200

    Rutados y router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TareasTablaComponent } from './tareas-tabla/tareas-tabla.component';
+import { TareaFormComponent } from './tarea-form/tarea-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tareas/tabla', component: TareasTablaComponent },
+  { path: 'tareas/form', component: TareaFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

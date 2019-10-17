91718aead81142b2fb21afc70fd674679de6f074
Javier Lete <javier@eskura21.com>
2019-10-17 20:44:02 +0200

    Petición getTareas desde REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TareasTablaComponent } from './tareas-tabla/tareas-tabla.component';
 import { TareaFormComponent } from './tarea-form/tarea-form.component';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tarea.service.ts
+++ src/app/tarea.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Tarea } from './tarea';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
+  url = 'http://localhost:3000/tareas';
   tareas: Tarea[];
 
+  constructor(private http: HttpClient) {}
+
   getTareas(): Observable<Tarea[]> {
-    return of(this.tareas);
+    return this.http.get<Tarea[]>(this.url);
   }
 }

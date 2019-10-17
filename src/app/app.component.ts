03cd81b028ea42074d67e897b736a468303e2ff4
Javier Lete <javier@eskura21.com>
2019-10-17 19:39:41 +0200

    Traslado de tabla a TareasTablaComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { Tarea } from './tarea';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tareas: Tarea[] = [
-    { id: 1, nombre: 'Tarea 1', descripcion: 'Descripción 1', terminada: false },
-    { id: 2, nombre: 'Tarea 2', descripcion: 'Descripción 2', terminada: true }
-  ];
-}
+export class AppComponent {}
--- src/app/tareas-tabla/tareas-tabla.component.ts
+++ src/app/tareas-tabla/tareas-tabla.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from '../tarea';
 
 @Component({
   selector: 'app-tareas-tabla',
   templateUrl: './tareas-tabla.component.html',
   styleUrls: ['./tareas-tabla.component.css']
 })
-export class TareasTablaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class TareasTablaComponent {
+  tareas: Tarea[] = [
+    { id: 1, nombre: 'Tarea 1', descripcion: 'Descripción 1', terminada: false },
+    { id: 2, nombre: 'Tarea 2', descripcion: 'Descripción 2', terminada: true }
+  ];
 }

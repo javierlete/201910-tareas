187517bf6a97d09161052aca1985851ada6e8db0
Javier Lete <javier@eskura21.com>
2019-10-17 20:24:43 +0200

    Formulario con enlace de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TareasTablaComponent } from './tareas-tabla/tareas-tabla.component';
 import { TareaFormComponent } from './tarea-form/tarea-form.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { TareaFormComponent } from './tarea-form/tarea-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tarea-form/tarea-form.component.ts
+++ src/app/tarea-form/tarea-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from '../tarea';
 
 @Component({
   selector: 'app-tarea-form',
   templateUrl: './tarea-form.component.html',
   styleUrls: ['./tarea-form.component.css']
 })
-export class TareaFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class TareaFormComponent {
+  tarea: Tarea = {
+    id: 100,
+    nombre: 'Tarea de prueba',
+    descripcion: 'Descripción de prueba',
+    terminada: true
+  };
 }
